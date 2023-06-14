259433109022dd6d5c4d387f6c3774e8bc371561
mohacelhosen <mohacel.hosen22@gmail.com>
2023-06-14 17:16:21 +0600

    Information Updated

--- src/main/java/com/mohacel/edu/dto/AuthorizedPickup.java
+++ src/main/java/com/mohacel/edu/dto/AuthorizedPickup.java
@@ -1,15 +1,9 @@
 package com.mohacel.edu.dto;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 public class AuthorizedPickup {
     // who will pick up your child
-
     private String fullName;
     private String gender;
     private Integer age;
